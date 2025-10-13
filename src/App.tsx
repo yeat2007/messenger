f8f64cd965ef45d4cad38c9de0e5fdf74bf03f5f
Arseniy <7558414@list.ru>
2025-10-13 19:05:47 +0300

    fix app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import './App.css'
 
-
 type MessageProps = {
   text: string;
   sent: boolean;
@@ -10,7 +9,7 @@ type MessageProps = {
 const Message = ({text, sent}: MessageProps) => {
   return ( 
     <div className={`message ${sent ? "sent" : ""}`}>
-    {text}
+      {text}
     </div>
   );
 }
@@ -22,31 +21,26 @@ type ChatItemProps = {
 }
 const ChatItem = ({name, time, message }: ChatItemProps) => {
   return (
-    <div className="chat-list">
-      <div className="chat-item">
-        <img src="https://placehold.co/50x50" className="avatar" alt={name} />
-        <div className="user-info">
-          <div className="chat-top">
-            <span className="user-name">{name}</span>
-            <span className="chat-time">{time}</span>
+    <div className="chatList">
+      <div className="chatItem">
+        <img src="https://placehold.co/50x50" className="avatar" alt={name}/>
+        <div className="userInfo">
+          <div className="chatTop">
+            <span className="userName">{name}</span>
+            <span className="chatTime">{time}</span>
           </div>
-          <div className="chat-message">{message}</div>
+          <div className="chatMessage">{message}</div>
         </div>
       </div>
     </div>
   );
-
 }
-
-
-
-
 
 function App() {
   const [messages, setMessages] = useState([
-    {text: "privet nigga", sent: false},
+    {text: "privet", sent: false},
     {text: "watsUP", sent: true},
-
+    {text: "ninja ", sent: false},
   ]);
   const [newMessage, setNewMessage] = useState("");
   const typeSend = () => {
@@ -61,11 +55,10 @@ function App() {
     {name: "user", time: "10:34", message: "hello"},
     {name: "qwerty", time: "14:09", message: "ninjajajajaja"},
     {name: "usasder", time: "19:12", message: "helasdasdasdlo"},
-
   ]
 
   return(
-  <div className="mainBlock">
+    <div className="mainBlock">
       <div className="navigation">
         <div className="chatContainer">
           {chats.map((chat, i) => (
@@ -73,29 +66,27 @@ function App() {
               key={i}
               name={chat.name}
               time={chat.time}
-              message={chat.message}
-            />
+              message={chat.message}/>
           ))}
         </div>
       </div>
 
       <div className="cardsContainer">
-        <div className="main-card">
+        <div className="mainCard">
           <div className="chatWithUser">
             <div className="messages">
               {messages.map((m, i) => (
-                <Message key={i} text={m.text} sent={m.sent} />
+                <Message key={i} text={m.text} sent={m.sent}/>
               ))}
             </div>
           </div>
 
-          <div className="message-type">
+          <div className="messageType">
             <textarea
               className="typing"
               placeholder="Type a message..."
               value={newMessage}
-              onChange={(e) => setNewMessage(e.target.value)}
-            />
+              onChange={(e) => setNewMessage(e.target.value)}/>
             <button className="sendMess" onClick={typeSend}>
               Send
             </button>
