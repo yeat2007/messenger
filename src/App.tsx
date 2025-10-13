85e7c3b9eb32100999a6285728909a9514008705
Arseniy <7558414@list.ru>
2025-10-13 18:44:12 +0300

    app logic

--- src/App.tsx
+++ src/App.tsx
@@ -43,11 +43,66 @@ const ChatItem = ({name, time, message }: ChatItemProps) => {
 
 
 function App() {
+  const [messages, setMessages] = useState([
+    {text: "privet nigga", sent: false},
+    {text: "watsUP", sent: true},
 
+  ]);
+  const [newMessage, setNewMessage] = useState("");
+  const typeSend = () => {
+    if(newMessage.trim()){
+      setMessages([...messages,
+        {text: newMessage, sent: true }
+      ]);
+      setNewMessage("");
+    }
+  };
+  const chats = [
+    {name: "user", time: "10:34", message: "hello"},
+    {name: "qwerty", time: "14:09", message: "ninjajajajaja"},
+    {name: "usasder", time: "19:12", message: "helasdasdasdlo"},
 
+  ]
 
   return(
+  <div className="mainBlock">
+      <div className="navigation">
+        <div className="chatContainer">
+          {chats.map((chat, i) => (
+            <ChatItem
+              key={i}
+              name={chat.name}
+              time={chat.time}
+              message={chat.message}
+            />
+          ))}
+        </div>
+      </div>
 
+      <div className="cardsContainer">
+        <div className="main-card">
+          <div className="chatWithUser">
+            <div className="messages">
+              {messages.map((m, i) => (
+                <Message key={i} text={m.text} sent={m.sent} />
+              ))}
+            </div>
+          </div>
+
+          <div className="message-type">
+            <textarea
+              className="typing"
+              placeholder="Type a message..."
+              value={newMessage}
+              onChange={(e) => setNewMessage(e.target.value)}
+            />
+            <button className="sendMess" onClick={typeSend}>
+              Send
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
