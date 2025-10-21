c88ef75a500c6fad8241b6ca7b406e4abe7a7547
Arseniy <7558414@list.ru>
2025-10-21 19:25:45 +0300

    update css & tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,140 @@
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
 import './App.css'
+
+
+
+type UsersProps = {
+  userName: string,
+  userMessage: string,
+  userAvatar?: string,
+  userOnline: boolean,
+  messageTime?: string,
+  lastMessage?: string,
+};
+
+
+const contacts: UsersProps[] = [
+  {
+    userName: "Vasya",
+    userMessage: "hello world('print')",
+    userAvatar: "https://placehold.co/50x50",
+    userOnline: true,
+    messageTime: "10:12",
+    lastMessage: "qwerty",
+  },
+
+  
+  {
+    userName: "Werty",
+    userMessage: "bye",
+    userAvatar: "https://placehold.co/50x50",
+    userOnline: true,
+    messageTime: "12:12",
+    lastMessage: "",
+  },
+
+
+  {
+    userName: "GHJ",
+    userMessage: "ninja, aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    userAvatar: "https://placehold.co/50x50",
+    userOnline: true,
+    messageTime: "20:12",
+    lastMessage: "!!!!!",
+  },
+
+
+  {
+    userName: "IO",
+    userMessage: "wtf",
+    userAvatar: "https://placehold.co/50x50",
+    userOnline: true,
+    messageTime: "23:12",
+    lastMessage: "qrty",
+  },
+  {
+    userName: "IO",
+    userMessage: "wtf",
+    userAvatar: "https://placehold.co/50x50",
+    userOnline: true,
+    messageTime: "23:12",
+    lastMessage: "qrty",
+  },
+  {
+    userName: "IO",
+    userMessage: "wtf",
+    userAvatar: "https://placehold.co/50x50",
+    userOnline: true,
+    messageTime: "23:12",
+    lastMessage: "qrty",
+  },
+  {
+    userName: "IO",
+    userMessage: "wtf",
+    userAvatar: "https://placehold.co/50x50",
+    userOnline: true,
+    messageTime: "23:12",
+    lastMessage: "qrty",
+  },
+  {
+    userName: "IO",
+    userMessage: "wtf",
+    userAvatar: "https://placehold.co/50x50",
+    userOnline: true,
+    messageTime: "23:12",
+    lastMessage: "qrty",
+  },
+  {
+    userName: "IO",
+    userMessage: "wtf",
+    userAvatar: "https://placehold.co/50x50",
+    userOnline: true,
+    messageTime: "23:12",
+    lastMessage: "qrty",
+  },
+  {
+    userName: "IO",
+    userMessage: "wtf",
+    userAvatar: "https://placehold.co/50x50",
+    userOnline: true,
+    messageTime: "23:12",
+    lastMessage: "qrty",
+  },
+  {
+    userName: "IO",
+    userMessage: "wtf",
+    userAvatar: "https://placehold.co/50x50",
+    userOnline: true,
+    messageTime: "23:12",
+    lastMessage: "qrty",
+  },
+  {
+    userName: "IO",
+    userMessage: "wtf",
+    userAvatar: "https://placehold.co/50x50",
+    userOnline: true,
+    messageTime: "23:12",
+    lastMessage: "qrty",
+  },
+]
+
+const ChatItem = ({userName, userMessage, userAvatar, messageTime }: UsersProps) => {
+  return (
+    <div className="chatList">
+      <div className="chatItem">
+        <img src = {userAvatar} className='avatar'></img>
+        <div className="userInfo">
+          <div className="chatTop">
+            <span className="userName">{userName}</span>
+            <span className="chatTime">{messageTime}</span>
+          </div>
+          <div className="chatMessage">{userMessage}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 
 type MessageProps = {
   text: string;
@@ -14,60 +149,77 @@ const Message = ({text, sent}: MessageProps) => {
   );
 }
 
-type ChatItemProps = {
-  name: string;
-  time: string;
-  message: string;
-}
-const ChatItem = ({name, time, message }: ChatItemProps) => {
-  return (
-    <div className="chatList">
-      <div className="chatItem">
-        <img src="https://placehold.co/50x50" className="avatar" alt={name}/>
-        <div className="userInfo">
-          <div className="chatTop">
-            <span className="userName">{name}</span>
-            <span className="chatTime">{time}</span>
-          </div>
-          <div className="chatMessage">{message}</div>
-        </div>
-      </div>
-    </div>
-  );
-}
+
 
 function App() {
-  const [messages, setMessages] = useState([
+  const [messages, setMessages] = useState<MessageProps[]>([
     {text: "privet", sent: false},
     {text: "watsUP", sent: true},
     {text: "ninja ", sent: false},
   ]);
+
   const [newMessage, setNewMessage] = useState("");
-  const typeSend = () => {
+  const sendMessage = () => {
     if(newMessage.trim()){
-      setMessages([...messages,
-        {text: newMessage, sent: true }
-      ]);
+      setMessages([...messages, {text: newMessage, sent: true }]);
       setNewMessage("");
     }
   };
-  const chats = [
-    {name: "user", time: "10:34", message: "hello"},
-    {name: "qwerty", time: "14:09", message: "ninjajajajaja"},
-    {name: "usasder", time: "19:12", message: "helasdasdasdlo"},
-  ]
+
+  const [time, setTime] = useState<string>('');
+  
+  useEffect(() => {
+    const updateTime = () => {
+      const now = new  Date();
+      const formattedTime = now.toLocaleTimeString([], {hour: '2-digit' , minute: '2-digit' })
+      setTime(formattedTime)
+      }
+  updateTime();
+  const timer = setInterval(updateTime, 1000);
+  return () => { clearInterval(timer)};
+} ,[]);
 
   return(
     <div className="mainBlock">
       <div className="navigation">
+        <div id = "timeDisplay">{time}</div>
+        <div className="messageTitle">
+          <img src="src/assets/Messages.svg"/>
+        </div>
+
+        <div className="search">
+          <input type="text" id="searchinput" placeholder="Search"/>
+          <button id = "searchBtn">
+            <img className="searchinput" src="src/assets/Search.svg"/>
+          </button>
+        </div>
+
+        <div className="buttons">
+          <button className="navBtn">All</button>
+          <button className="navBtn">Groups</button>
+          <button className="navBtn">All</button>
+        </div>
+
         <div className="chatContainer">
-          {chats.map((chat, i) => (
-            <ChatItem
-              key={i}
-              name={chat.name}
-              time={chat.time}
-              message={chat.message}/>
+          {contacts.map((contact, i) => (
+            <ChatItem key={i} {...contact}/>
           ))}
+        </div>
+        <div className="btnsContainer">
+          <div className="btns">
+            <button className="bottomBtn">
+              <img src="src/assets/Phone.svg" alt="phone" />
+              <span>Calls</span>
+            </button>
+            <button className="bottomBtn">
+              <img src="src/assets/Chat Message.svg" alt="chat" />
+              <span>Chats</span>
+            </button>
+            <button className="bottomBtn">
+              <img src="src/assets/Settings.svg" alt="settings" />
+              <span>Settings</span>
+            </button>
+          </div>
         </div>
       </div>
 
@@ -75,19 +227,20 @@ function App() {
         <div className="mainCard">
           <div className="chatWithUser">
             <div className="messages">
-              {messages.map((m, i) => (
-                <Message key={i} text={m.text} sent={m.sent}/>
-              ))}
+              {messages.map ((m, i) => (
+                <Message key = {i} text = {m.text} sent = {m.sent}/>
+                ))}
+              <div className="gradientCircle"></div>
             </div>
           </div>
 
           <div className="messageType">
             <textarea
               className="typing"
-              placeholder="Type a message..."
+              placeholder="Type a message ..."
               value={newMessage}
               onChange={(e) => setNewMessage(e.target.value)}/>
-            <button className="sendMess" onClick={typeSend}>
+            <button className="sendMess" onClick={sendMessage}>
               Send
             </button>
           </div>
